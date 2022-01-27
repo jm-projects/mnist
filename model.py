fe7346447a84a4c9d56f1803ed8ed527a8f19f8f
J Marza <joshua.marza@stcatz.ox.ac.uk>
2022-01-27 22:35:38 +0000

    PEP-8 compliance

--- model.py
+++ model.py
@@ -1,37 +1,40 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten, Dropout
+from tensorflow.keras.layers import (Input, Dense, Conv2D,
+                                     MaxPool2D, Flatten, Dropout)
 from tensorflow.keras.models import Model
 
 
 def creat_model():
 
     model_activation = tf.nn.relu
-    input_shape = (28,28,1)
+    input_shape = (28, 28, 1)
     input_layer = Input(input_shape)
-    layer = Conv2D(32, (5,5),
+    layer = Conv2D(32, (5, 5),
                    activation=model_activation,
                    padding='same',
                    input_shape=input_shape)(input_layer)
-    layer = MaxPool2D((2,2))(layer)
-    layer = Conv2D(64,(3,3), activation=tf.nn.relu, padding='same')(layer)
-    layer = Conv2D(64,(3,3), activation=tf.nn.relu, padding='same')(layer)
-    layer = MaxPool2D((2,2))(layer)
-    layer = Conv2D(128,(3,3), activation=tf.nn.relu, padding='same')(layer)
-    layer = Conv2D(128,(3,3), activation=tf.nn.relu, padding='same')(layer)
-    layer = Conv2D(128,(3,3), activation=tf.nn.relu, padding='same')(layer)
-    layer = MaxPool2D((2,2))(layer)
+    layer = MaxPool2D((2, 2))(layer)
+    layer = Conv2D(64, (3, 3), activation=tf.nn.relu, padding='same')(layer)
+    layer = Conv2D(64, (3, 3), activation=tf.nn.relu, padding='same')(layer)
+    layer = MaxPool2D((2, 2))(layer)
+    layer = Conv2D(128, (3, 3), activation=tf.nn.relu, padding='same')(layer)
+    layer = Conv2D(128, (3, 3), activation=tf.nn.relu, padding='same')(layer)
+    layer = Conv2D(128, (3, 3), activation=tf.nn.relu, padding='same')(layer)
+    layer = MaxPool2D((2, 2))(layer)
 
     flatten = Flatten()(layer)
 
     layer = Dense(512, activation=tf.nn.relu)(flatten)
     layer = Dropout(0.25)(layer)
     layer = Dense(512, activation=tf.nn.relu)(layer)
-    layer  = Dropout(0.25)(layer)
+    layer = Dropout(0.25)(layer)
     layer = Dense(512, activation=tf.nn.relu)(layer)
-    layer  = Dropout(0.25)(layer)
+    layer = Dropout(0.25)(layer)
     output_layer = Dense(10, activation=tf.nn.softmax)(layer)
-    model = Model(input_layer,output_layer)
-    
-    model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    
+    model = Model(input_layer, output_layer)
+
+    model.compile(optimizer='Adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
     return model
--- training.py
+++ training.py
@@ -1,6 +1,3 @@
-import os
-import argparse
-
 import pandas as pd
 import numpy as np
 from tensorflow import keras
@@ -12,38 +9,45 @@ from model import creat_model
 if __name__ == "__main__":
 
     """ Data preprocessing:
-    Here we simply import the data and scale/reshape it so each pixel has a value
-    between 0 and 1, and it is in a 28x28x1 numpy array.
-    """
+    Here we simply import the data and scale/reshape it so each
+    pixel has a value between 0 and 1, and it is in a 28x28x1
+    numpy array"""
+
     train = pd.read_csv('data/train.csv')
 
-    x_digit = train.drop(['label'], axis = 1) # Set the image data as 'x_axis'
-    x_digit = np.array(x_digit, dtype=np.float32)/255 
+    # Set the image data as 'x_axis'
+    x_digit = train.drop(['label'], axis=1)
+    x_digit = np.array(x_digit, dtype=np.float32)/255
     x_digit = x_digit.reshape(-1, 28, 28, 1)
 
     y_digit = train['label']
-    y_digit = keras.utils.to_categorical(y_digit, num_classes = 10)
+    y_digit = keras.utils.to_categorical(y_digit, num_classes=10)
 
     """ Data augmentation:
     Here we use tensorflows ImageDataGenerator to augment the data.
-    This is done in batches of 256 for the test data and 64 for the validation data
-    """
-    datagen = ImageDataGenerator(rotation_range=20,
-                    width_shift_range=0.20,
-                    shear_range=15,
-                    zoom_range=0.10,
-                    validation_split=0.25,
-                    horizontal_flip=False)
+    This is done in batches of 256 for the test data and 64 for
+    the validation data"""
+
+    datagen = ImageDataGenerator(
+        rotation_range=20,
+        width_shift_range=0.20,
+        shear_range=15,
+        zoom_range=0.10,
+        validation_split=0.25,
+        horizontal_flip=False)
 
     datagen.fit(x_digit)
 
-    gen_train  = datagen.flow(x_digit,y_digit,batch_size=256,subset='training')
-    gen_validation = datagen.flow(x_digit,y_digit,batch_size=64,subset='validation')
+    gen_train = datagen.flow(x_digit, y_digit, batch_size=256,
+                             subset='training')
+    gen_validation = datagen.flow(x_digit, y_digit, batch_size=64,
+                                  subset='validation')
 
     """ Model fit:
     Now we create the model as specified in model.py.
-    Checkpoints are used to save the training weights of the best fit.
-    """
+    Checkpoints are used to save the training weights of the best
+    fit."""
+
     model = creat_model()
     model.summary()
 
@@ -55,13 +59,14 @@ if __name__ == "__main__":
                                   verbose=1)
 
     from tensorflow.keras.callbacks import ModelCheckpoint
-    checkpoint = ModelCheckpoint(filepath='digit-recognizer-model.hdf5',
-                                 monitor='val_loss',
-                                 save_best_only=True,
-                                 save_weights_only=True,verbose=1)
+    checkpoint = ModelCheckpoint(
+        filepath='digit-recognizer-model.hdf5',
+        monitor='val_loss',
+        save_best_only=True,
+        save_weights_only=True, verbose=1)
 
     model_hist = model.fit(gen_train,
-                        validation_data = gen_validation,
-                        epochs=40,
-                        callbacks=[reduce_lr,checkpoint],
-                        verbose=1)
+                           validation_data=gen_validation,
+                           epochs=40,
+                           callbacks=[reduce_lr, checkpoint],
+                           verbose=1)
